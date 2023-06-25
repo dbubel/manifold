c226714dd058194397688d12958558ead62c1006
dbubel <50341559+dbubel@users.noreply.github.com>
2023-06-25 11:57:16 -0400

    basic sharded blocking

--- shards/shards_test.go
+++ shards/shards_test.go
@@ -1,7 +1,9 @@
 package shards
 
 import (
+	"context"
 	"testing"
+	"time"
 )
 
 func TestShardedDataBasic(t *testing.T) {
@@ -31,5 +33,43 @@ func TestShardedDataTwoShards(t *testing.T) {
 	}
 	if string(dq) != "Hello World!" {
 		t.Errorf("Expected: %v, got: %v", "test", string(dq))
+	}
+}
+
+func TestShardedBlockingDequeue(t *testing.T) {
+	data := NewShardedTopics(1)
+	go func() {
+		time.Sleep(time.Second)
+		err := data.Enqueue("test", []byte("Hello World!"))
+		if err != nil {
+			t.Errorf("Error: %v", err)
+		}
+	}()
+
+	d, err := data.BlockingDequeue(context.Background(), "test")
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	if string(d) != "Hello World!" {
+		t.Errorf("Expected: %v, got: %v", "test", string(d))
+	}
+}
+
+func TestShardedBlockingDequeueMultipleShards(t *testing.T) {
+	data := NewShardedTopics(5)
+	go func() {
+		time.Sleep(time.Second)
+		err := data.Enqueue("test", []byte("Hello World!"))
+		if err != nil {
+			t.Errorf("Error: %v", err)
+		}
+	}()
+
+	d, err := data.BlockingDequeue(context.Background(), "test")
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	if string(d) != "Hello World!" {
+		t.Errorf("Expected: %v, got: %v", "test", string(d))
 	}
 }
